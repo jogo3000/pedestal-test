f224368cc909be6aae907f14d8de4a01398f1feb
Matti Uusitalo <uusitalo.matti@gmail.com>
2025-02-02 06:54:06 +0200

    Send keepalive messages from client

--- resources/public/ws.js
+++ resources/public/ws.js
@@ -1,10 +1,38 @@
+const KEEPALIVE_RATE = 10000;
 const ws = new WebSocket("ws://localhost:8890/ws");
+let timeoutId = null;
+
+
+const keepalive = () => {
+    if (timeoutId) {
+        console.log("Sending ping");
+        ws.send("ping");
+        timeoutId = setTimeout(keepalive, KEEPALIVE_RATE);
+    }
+}
 
 ws.onopen = (event) => {
     ws.send("Connected to the server");
+
+    timeoutId = setTimeout(keepalive, KEEPALIVE_RATE);
 }
+
+ws.onclose = (event) => {
+    clearTimeout(timeoutId);
+    timeoutId = null;
+}
+
 
 ws.onmessage = (event) => {
     console.log("Event from server!", event.data);
     const msg = JSON.parse(event.data);
+
+    switch (msg.type) {
+    case "REPLACE":
+        const el = document.getElementById(msg.id)
+        el.setHTMLUnsafe(msg.html)
+        break;
+    default:
+        console.log(`Unknown message type ${msg.type}`)
+    }
 }
